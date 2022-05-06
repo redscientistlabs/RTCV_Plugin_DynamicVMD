46e8f487e9c92bda2ae20f71df4dfeb3aa77bac4
Phil Girard <phil@redscientist.com>
2022-05-05 22:38:39 -0400

    Mega Warning Cleanup

--- RTCV_Plugin_DynamicVMD/RTCV_Plugin_DynamicVMD/UI/PluginForm.cs
+++ RTCV_Plugin_DynamicVMD/RTCV_Plugin_DynamicVMD/UI/PluginForm.cs
@@ -39,18 +39,10 @@ namespace DYNAMICVMD.UI
 
         Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
-        List<(string, BlastLayer)> loadedBlastLayers = null;
-        Size DefaultSize;
-
-        private bool bCancel = false;
-        private string gameID;
-        private static string flatGameId = "000000000";
         WebClient wc = new WebClient();
 
         //This dictionary will inflate forever but it would take quite a while to be noticeable.
         Dictionary<string, bool> encounteredIds = new Dictionary<string, bool>();
-        private string gameIdUrl;
-        private System.Windows.Forms.Timer ticker = null;
 
         private long currentDomainSize = 0;
 
